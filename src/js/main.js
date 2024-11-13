c3fde69ad0b6ace5395692e980fb92589ad7d0c8
slaus <mister.slaus@gmail.com>
2024-11-13 02:12:17 +0200

    desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -24,4 +24,71 @@ const smoothScroll = (selector, target) => {
 smoothScroll('.header__logo--link', '#header');
 smoothScroll('.header__btn', '#contacts');
 smoothScroll('.intro__text--btn', '#contacts');
+smoothScroll('.situation__btn', '#contacts');
+smoothScroll('.consult__btn', '#contacts');
 smoothScroll('.candidat__btn', '#about');
+
+
+const swiperTeam = new Swiper('.swiper-team', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    mousewheel: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        },
+    },
+    on: {
+        click: function () {
+            this.slideNext();
+        }
+    }
+});
+
+const swiperClients = new Swiper('.swiper-clients', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    mousewheel: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        1199: {
+            slidesPerView: 2,
+            spaceBetween: 50
+        },
+    },
+    on: {
+        click: function () {
+            this.slideNext();
+        }
+    }
+});
