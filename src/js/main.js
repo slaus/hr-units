d85817fc07afb27068b61b9b3f18ff540f7efff3
slaus <mister.slaus@gmail.com>
2024-11-14 20:25:39 +0200

    added clients and phone mask

--- src/js/main.js
+++ src/js/main.js
@@ -88,3 +88,30 @@ const swiperClients = new Swiper('.swiper-clients', {
         }
     }
 });
+
+document.getElementById("phone").addEventListener("input", function(e) {
+    let input = e.target;
+    let value = input.value.replace(/\D/g, "");
+
+    if (value === "") {
+      input.value = "";
+      return;
+    }
+
+    let formattedValue = "+7 (";
+
+    if (value.length > 1) {
+      formattedValue += value.substring(1, 4);
+    }
+    if (value.length >= 5) {
+      formattedValue += ") " + value.substring(4, 7);
+    }
+    if (value.length >= 8) {
+      formattedValue += "-" + value.substring(7, 9);
+    }
+    if (value.length >= 10) {
+      formattedValue += "-" + value.substring(9, 11);
+    }
+
+    input.value = formattedValue;
+  });
